481574799c4c15bbc6563c087c91b79a7df7f8ec
Kortnev Dmitriy <kortnevdmitriy@outlook.com>
2017-08-30 21:32:42 +0300

    Malipo payment app. Implements carrier billing. Configured to charge only Kenyan mobile operators.

--- app/src/main/java/ai/kortnevdmitriy/malipo/MainActivity.java
+++ app/src/main/java/ai/kortnevdmitriy/malipo/MainActivity.java
@@ -4,11 +4,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.stripe.android.Stripe;
+import com.stripe.android.TokenCallback;
+import com.stripe.android.model.Card;
+import com.stripe.android.model.Token;
+
+import io.fabric.sdk.android.Fabric;
 import mp.MpUtils;
 import mp.PaymentRequest;
 import mp.PaymentResponse;
@@ -21,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -78,6 +88,44 @@ public class MainActivity extends AppCompatActivity {
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
+
+    /*----------------------------------------------------------------------------------------------
+    *  ACCEPTING CREDIT CARD PAYMENT USING STRIPE API
+    -----------------------------------------------------------------------------------------------*/
+    public void submitCard(View view) {
+        // TODO: replace with your own test key
+        final String publishableApiKey = BuildConfig.DEBUG ?
+                "pk_test_6pRNASCoBOKtIshFeQd4XMUh" :
+                getString(R.string.com_stripe_publishable_key);
+
+        TextView cardNumberField = (TextView) findViewById(R.id.cardNumber);
+        TextView monthField = (TextView) findViewById(R.id.month);
+        TextView yearField = (TextView) findViewById(R.id.year);
+        TextView cvcField = (TextView) findViewById(R.id.cvc);
+
+        Card card = new Card(cardNumberField.getText().toString(),
+                Integer.valueOf(monthField.getText().toString()),
+                Integer.valueOf(yearField.getText().toString()),
+                cvcField.getText().toString());
+
+        Stripe stripe = new Stripe();
+        stripe.createToken(card, publishableApiKey, new TokenCallback() {
+            public void onSuccess(Token token) {
+                // TODO: Send Token information to your backend to initiate a charge
+                Toast.makeText(
+                        getApplicationContext(),
+                        "Token created: " + token.getId(),
+                        Toast.LENGTH_LONG).show();
+            }
+
+            public void onError(Exception error) {
+                Log.d("Stripe", error.getLocalizedMessage());
+            }
+        });
+    /*---------------------------------------------------------------------------------------------*/
+
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
